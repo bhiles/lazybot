856d51c8daab10426eda60f8ced687b2f4615174
bhiles <bennetthiles@gmail.com>
2012-04-25 10:36:54 -0700

    lightly simplified functions in the hello-world test

--- test/lazybot/plugins/hello_world_test.clj
+++ test/lazybot/plugins/hello_world_test.clj
@@ -4,23 +4,24 @@
         [lazybot.plugins.hello-world] :reload-all)
   (:use [clojure.test]))
 
-(deftest test-hello-world
+(deftest test-basic
   (let [bot (ref {})]
     (load-this-plugin nil bot)
     (defn mock-send-message [com-m s & args]
-      (is (= "Hello, World!" s)))
+      (is (= "Hello, World!" s))
+      true)
     (binding [lazybot.registry/send-message mock-send-message]
       (let [resp (respond {:bot bot :command "hiworld"})]
-        (is (= true (resp nil)))))))
+        (is (true? (resp nil)))))))
 
 (deftest test-not-full-trigger
   (let [bot (ref {})]
     (load-this-plugin nil bot)
       (let [resp (respond {:bot bot :command "hiworl"})]
-        (is (= nil (resp nil))))))
+        (is (nil? (resp nil))))))
 
 (deftest test-more-than-trigger
   (let [bot (ref {})]
     (load-this-plugin nil bot)
     (let [resp (respond {:bot bot :command "hiworlds"})]
-      (is (= nil (resp nil))))))
+      (is (nil? (resp nil))))))
