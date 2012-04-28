6564ce79530810e0a39cb17404546bc1127098ce
bhiles <bennetthiles@gmail.com>
2012-04-28 14:33:03 -0700

    moved autoreply test logic into functions

--- test/lazybot/plugins/autoreply_test.clj
+++ test/lazybot/plugins/autoreply_test.clj
@@ -11,17 +11,42 @@
                 {#".*(https?://)richhickey(.github.com/\S*).*"
                  "Nooooo, that's so out of date! Please see instead $1clojure$2 and try to stop linking to rich's repo."}}}}})
 
-(deftest test-basic
-  (let [bot (ref {})
-        com (ref {:server "irc.freenode.net"})]
-    (load-this-plugin nil bot)
-    (dosync (alter bot assoc :config config))
-    (defn mock-send-message [com-m s & args]
-      (is (= "Nooooo, that's so out of date! Please see instead https://clojure.github.com/takemethere/zomg.html and try to stop linking to rich's repo." s))
-      true)
-    (binding [lazybot.registry/send-message mock-send-message]
+(defn assert-valid
+  "Asserts the plugin responds correctly."
+  ([message expected-response] (assert-valid "irc.freenode.net" "#tempchan" message expected-response))
+  ([irc-name channel message expected-response]
+    (let [bot (ref {})
+          com (ref {:server irc-name})]
+      (load-this-plugin nil bot)
+      (dosync (alter bot assoc :config config))
+      (defn mock-send-message [com-m s & args]
+        (is (= expected-response s))
+        true)
+      (binding [lazybot.registry/send-message mock-send-message]
+        (let [hook-resp (pull-hooks bot :on-message)]
+          (is (true? ((first hook-resp) {:bot bot
+                                         :com com
+                                         :channel channel
+                                         :message message}))))))))
+
+(defn assert-invalid
+  "Asserts the plugin doesn't respond"
+  ([message] (assert-invalid "irc.freenode.net" "#tempchan" message ))
+  ([irc-name channel message]
+    (let [bot (ref {})
+          com (ref {:server "irc.freenode.net"})]
+      (load-this-plugin nil bot)
+      (dosync (alter bot assoc :config config))
       (let [hook-resp (pull-hooks bot :on-message)]
-        (is (true? ((first hook-resp) {:bot bot
-                                       :com com
-                                       :channel "#tempchan"
-                                       :message "https://richhickey.github.com/takemethere/zomg.html"})))))))
+        (is (nil? ((first hook-resp) {:bot bot
+                                      :com com
+                                      :channel "#tempchan"
+                                      :message "not a message that has a reply"})))))))
+
+(deftest test-basic
+  (let [message "https://richhickey.github.com/takemethere/zomg.html"
+        expected-response "Nooooo, that's so out of date! Please see instead https://clojure.github.com/takemethere/zomg.html and try to stop linking to rich's repo."]
+    (assert-valid message expected-response)))
+
+(deftest test-no-reponse
+  (assert-invalid "not a message that has a reply"))
