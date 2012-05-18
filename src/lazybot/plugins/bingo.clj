b33f6b5653d884d85b8cda2b7ea4203b84688cb8
Bennett Hiles <bennetthiles@gmail.com>
2012-05-17 19:18:49 -0700

    added bingo commands
    - added tests but they fail

--- src/lazybot/plugins/bingo.clj
+++ src/lazybot/plugins/bingo.clj
@@ -4,6 +4,10 @@
 (def words (ref #{"hi" "bye"}))
 (def remaining-words (ref #{"hi" "bye"}))
 
+(defn reset []
+  "Reset the bingo card"
+  (dosync (ref-set remaining-words @words)))
+
 (defplugin
   (:hook
     :on-message
@@ -19,7 +23,22 @@
               (ref-set remaining-words (into #{} (remove #{word} @remaining-words))))
             (println "remainign words after " @remaining-words)
             (if (empty? @remaining-words)
-              (send-message com-m (str nick "!!! You just got BINGO!!! You should probably read a book and learn some new words"))
-              (send-message com-m (str "Found " word " on " nick "'s buzzword bingo card")))))))))
-
-
+              (do
+                (send-message com-m (str nick "!!! You just got BUZZWORD BINGO!!! You should probably read a book and learn some new words"))
+                (reset))
+              (send-message com-m (str "Found " word " on " nick "'s buzzword bingo card"))))))))
+  (:cmd
+    "Creates a bingo card from a set of words"
+    #{"create-card"}
+    (fn [{:keys [com bot channel args] :as com-m}]
+      (let [new-words (set args)]
+        (dosync
+          (ref-set words (into #{} new-words))
+          (ref-set remaining-words @words))
+        (send-message com-m "New bingo card created"))))
+  (:cmd
+    "Resets a bingo card so that no words have been crossed off"
+    #{"reset-card"}
+    (fn [com-m]
+      (reset)
+      (send-message com-m "Bingo card has been reset"))))
--- test/lazybot/plugins/bingo_test.clj
+++ test/lazybot/plugins/bingo_test.clj
@@ -32,3 +32,34 @@
       (let [hook-resp (pull-hooks bot :on-message)]
         (is (nil? ((first hook-resp) {:message "bye there"
                                       :nick "bennett"})))))))
+
+(deftest test-after-bingo
+  (test-all-words)
+  (let [bot (ref {})]
+    (defn mock-send-message-first [com-m s & args]
+      (is (= "Found hi on bennett's buzzword bingo card" s))
+      true)
+    (binding [lazybot.registry/send-message mock-send-message-first]
+      (let [hook-resp (pull-hooks bot :on-message)]
+        (is (nil? ((first hook-resp) {:message "hi there"
+                                      :nick "bennett"})))))))
+
+(deftest test-create-card
+  (let [bot (ref {})]
+    (load-this-plugin nil bot)
+    (defn mock-send-message-first [com-m s & args]
+      (is (= "New bingo card created" s))
+      true)
+    (binding [lazybot.registry/send-message mock-send-message-first]
+      (let [resp (respond {:bot bot :command "create-card" })]
+        (is (true? (resp {:bot bot :args ["a" "b"]})))))))
+
+(deftest test-reset-card
+  (let [bot (ref {})]
+    (load-this-plugin nil bot)
+    (defn mock-send-message-first [com-m s & args]
+      (is (= "Bingo card has been reset" s))
+      true)
+    (binding [lazybot.registry/send-message mock-send-message-first]
+      (let [resp (respond {:bot bot :command "reset-card" })]
+        (is (true? (resp {})))))))
